e6566b153c5a60f0ecf316076066b698624bb80c
nickforsman <niclas.forsman@outlook.com>
2015-08-05 21:38:27 +0300

    Adds application calendar module

---
+++ src/app/calendar/CalendarFactory.js
@@ -0,0 +1,57 @@
+;(function() {
+	'use strict';
+
+	var calendarFactory = function($http, User) {
+
+        function list() {
+            var promise = $http.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', { 
+                headers : {
+                    'Authorization': 'Bearer ' + User.accessToken 
+                } 
+            });
+
+            promise.success(function(data) {
+                return data;
+            });
+
+            promise.error(function(error) {
+                return error;
+            });
+
+            return promise;
+        }
+
+        function create(event) {
+            var promise = $http.post('https://www.googleapis.com/calendar/v3/calendars/primary/events', event, {
+                headers : {
+                    'Authorization': 'Bearer ' + User.accessToken 
+                }
+            });
+            
+            promise.success(function(payload) {
+                return payload;
+            });
+
+            promise.error(function(err) {
+                return err;
+            });
+
+            return promise;
+        }
+        
+        var calendar = {
+            list: list,
+            create: create
+        };
+
+        return calendar;
+	};
+
+	angular.module('CalendarApp')
+
+	.factory('calendarFactory', [
+        '$http',
+        'User',
+        calendarFactory
+    ]);
+})();
---
+++ src/app/calendar/calendar.js
@@ -0,0 +1,48 @@
+;(function() {
+	'use strict';
+
+	var CalendarController = function(calendarFactory, $exceptionHandler) {
+		var vm = this;
+		vm.eventSources = [[]];
+
+		vm.init = function() {
+			vm.list();
+		};
+		
+		vm.list = function() {
+			calendarFactory.list().success(function(data) {
+				data.items.forEach(function(item) {
+					if (item.status !== 'cancelled') {
+						vm.eventSources[0].push({
+							title: item.summary,
+							start: item.start.dateTime || item.start.date,
+							end: item.end.dateTime || item.end.date,
+							stick: true,
+						});	
+					}
+				});
+			}).error(function(err) {
+				$exceptionHandler(err);
+			});
+		};
+		
+		vm.init();
+	};
+
+
+	angular.module('CalendarApp')
+
+	.config(function($routeProvider) {
+        $routeProvider.when('/calendar', {
+            templateUrl: 'src/app/calendar/calendar.html',
+            controller: 'CalendarController',
+            controllerAs: 'calendar',
+        });
+	})
+
+	.controller('CalendarController', [
+		'calendarFactory',
+		'$exceptionHandler',
+		CalendarController
+	]);
+})();
