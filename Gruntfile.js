02190de6dc73eb2c098b3fa0be17d498797f9c46
nickforsman <niclas.forsman@outlook.com>
2015-08-07 17:53:56 +0300

    Edits css concationation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-karma');
 
@@ -14,16 +15,48 @@ module.exports = function(grunt) {
                 src: ['src/**/*.js'],
                 dest: 'build/js/app.js',
             },
+            css: {
+                src: [
+                    'build/css/normalize.css',
+                    'build/css/style.css',
+                    'bower_components/fullcalendar/dist/fullcalendar.css'
+                ],
+                dest: 'build/css/main.css', 
+            }
         },
         uglify: {
-            files: {
-                'build/js/*.js' : 'build/js/app.min.js',
+            options: {
+                mangle: false
+            },
+            app: {
+                files: {
+                    'build/js/app.min.js': ['build/js/app.js']
+                }   
+            },
+            vendor: {
+                files: {
+                    'build/js/vendor.min.js': [
+                        'bower_components/angular/angular.js', 
+                        'bower_components/angular-route/angular-route.js', 
+                        'bower_components/jquery/dist/jquery.min.js', 
+                        'bower_components/moment/min/moment.min.js', 
+                        'bower_components/angular-ui-calendar/src/calendar.js', 
+                        'bower_components/fullcalendar/dist/fullcalendar.js', 
+                        'bower_components/fullcalendar/dist/gcal.js'
+                    ]
+                }
             }
         },
         less: {
             files: {
                 src: ['src/less/main.less'],
                 dest: 'build/css/style.css',
+            }
+        },
+        cssmin: {
+            files: {
+                src: 'build/css/main.css',
+                dest:'build/css/style.min.css'
             }
         },
         karma: {
