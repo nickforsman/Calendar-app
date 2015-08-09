3ef1f7f0290fafe9f9e3f023b2c567574d4c9fb4
nickforsman <niclas.forsman@outlook.com>
2015-08-09 16:14:36 +0300

    Adsd GAE static directory names

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,9 +1,25 @@
+/**
+ * Login Controller
+ * @namespace Controllers
+ */
 ;(function() {
 	'use strict';
 
+	/**
+	 * LoginController handles logging in and rendering login template
+	 * @param {Object} firebaseService
+	 * @param {Object} Angular $location object
+	 * @param {Object} $exceptionHandler
+	 */
 	var LoginController = function(firebaseService, $location, $exceptionHandler) {
+		// scope this keyword to vm
 		var vm = this;
 
+		/**
+		 * Login, tries to log user in
+		 * firebaseSerive handles google auth scopes
+		 * @throws {Exception} If user could not be logged in
+		 */
 		vm.login = function() {
 			firebaseService.googleAuth().then(function(payload) {
 				$location.path('/calendar');
@@ -18,7 +34,7 @@
 
 	.config(function($routeProvider) {
         $routeProvider.when('/login', {
-            templateUrl: 'src/app/login/login.html',
+            templateUrl: '/templates/login.html',
             controller: 'LoginController',
             controllerAs: 'login',
         });
