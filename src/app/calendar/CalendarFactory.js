511d627933d6c250b2b68062537a1f7630c5a9ff
nickforsman <niclas.forsman@outlook.com>
2015-08-09 16:16:28 +0300

    Adds js doc to code

--- src/app/calendar/CalendarFactory.js
+++ src/app/calendar/CalendarFactory.js
@@ -1,9 +1,24 @@
+/**
+ * Calendar Factory
+ * @namespace Factories
+ */
 ;(function() {
 	'use strict';
 
+    /**
+     * CalendarFactory handles listing and creating events
+     * @param  {Object} $http
+     * @param  {Object} User constant
+     * @return {Object} FactoryObject
+     */
 	var calendarFactory = function($http, User) {
 
+        /**
+         * List, gets all the events for the current user
+         * @return {Object} $http promise
+         */
         function list() {
+            // store http result in variable
             var promise = $http.get('https://www.googleapis.com/calendar/v3/calendars/primary/events', { 
                 headers : {
                     'Authorization': 'Bearer ' + User.accessToken 
@@ -21,6 +36,11 @@
             return promise;
         }
 
+        /**
+         * Creates a new event with the given object param
+         * @param  {Object} Specific event object
+         * @return {Object} $http promise
+         */
         function create(event) {
             var promise = $http.post('https://www.googleapis.com/calendar/v3/calendars/primary/events', event, {
                 headers : {
@@ -39,6 +59,10 @@
             return promise;
         }
         
+        /**
+         * Exposes the methods for this factory
+         * @type {Object}
+         */
         var calendar = {
             list: list,
             create: create,
--- src/components/FirebaseService.js
+++ src/components/FirebaseService.js
@@ -1,20 +1,46 @@
+/**
+ * Firebase Service
+ * @namespace Services
+ */
 ;(function() {
 	'use strict';
 	
+	/**
+	 * FirebaseService handles login, logout, Oauth and checks if user is logged in
+	 * @param  {Object} Angular $location object
+	 * @param  {String} Firebase URL constant
+	 * @param  {Object} FirebaseAuth object
+	 */
 	function firebaseService($location, FBURL, $firebaseAuth) {
+		// Creates a new reference connection to firebase
 		var ref = new Firebase(FBURL);
+		// Creates a new firebase authentication reference
 		var auth = $firebaseAuth(ref);
 
+		/**
+		 * Checks if user is loggedIn
+		 * @return {Boolean && Object} 
+		 */
 		this.isLoggedIn = function() {
             return ref.getAuth();
         };
 
+        /**
+         * Logs user out of current auth reference
+         * And redirects back to login
+         */
         this.logout = function() {
         	ref.unauth();
         	$location.path('/login');
         };
 
+        /**
+         * Handles google authentication for the app
+         * @return {Object} FirebaseAuth promise
+         */
 		this.googleAuth = function() {
+			// Saves auth result in login var
+			// Scope: scopes for google auth
 			var login = auth.$authWithOAuthPopup("google", {
 				scope: [
 					'https://www.googleapis.com/auth/calendar',
